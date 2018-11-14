e4665541cbab3d6c74446af4e726fa28a6e3996a
Ripal Patel <ripal212007@gmail.com>
2018-11-13 21:31:31 -0600

    working

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import firebase from 'firebase/app';
 import apiKeys from '../db/apiKeys.json';
 import createNavbar from './components/navbar/navbar';
 import loginButton from './components/auth/auth';
+import checkLoginStatus from './helpers/authHelpers';
 import 'bootstrap';
 import './index.scss';
 
 const initializeApp = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   createNavbar();
+  checkLoginStatus();
   loginButton();
 };
 initializeApp();
