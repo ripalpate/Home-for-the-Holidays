bf2fecbb784179c75eb3b0e027b6bedf09d60cff
Ripal Patel <ripal212007@gmail.com>
2018-11-13 20:09:12 -0600

    login button works

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -2,10 +2,14 @@ import $ from 'jquery';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import './auth.scss';
+import googleImage from './signin_button.png';
 
 const loginButton = () => {
   const domString = `
- <button id="google-auth" class= "btn btn-secondary">login</button>
+ <button id="google-auth" class= "btn btn-secondary">
+  <img src="${googleImage}" alt="login button">
+ </button>
+
  `;
   $('#auth').html(domString);
   $('#google-auth').on('click', () => {
