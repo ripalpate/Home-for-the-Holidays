fe56e95efe4b8edc98542f01618bbaad658ea597
Ripal Patel <ripal212007@gmail.com>
2018-11-19 20:12:11 -0600

    single friend is printing

--- src/components/friendsPage/friendsPage.js
+++ src/components/friendsPage/friendsPage.js
@@ -3,7 +3,37 @@ import $ from 'jquery';
 import apiKeys from '../../../db/apiKeys.json';
 import authHelpers from '../../helpers/authHelpers';
 
+const printSingleFriend = (friend) => {
+  const friendString = `
+  <div>
+    <h1>${friend.name}</h1>
+    <h3>${friend.relationship}</h3>
+    <p>${friend.address}</p>
+    <p>${friend.email}</p>
+    <p>${friend.phoneNumber}</p>
+    <button class="btn btn-danger delete-button">X</button>
+  </div>
+  `;
+  $('#single-container').html(friendString);
+};
 
+const getSingleFriend = (e) => {
+  // firebase id(get from friend object that is in buildDropdown)
+  const friendId = e.target.dataset.dropdownId;
+  // dataset.dropdownID-comes from data-dropdown-id
+  // console.log(friendId);
+  axios.get(`${apiKeys.firebaseKeys.databaseURL}/friends/${friendId}.json`)
+    .then((result) => {
+      // singlefriend is coming directly from firebase object and
+      // doesn't have an id so we need to set it everytime.
+      const singleFriend = result.data;
+      // setting back id again
+      singleFriend.id = friendId;
+      printSingleFriend(singleFriend);
+    }).catch((error) => {
+      console.error('error in getting friend', error);
+    });
+};
 const buildDropdown = (friendsArray) => {
   let dropdown = `<div class="dropdown">
   <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -11,7 +41,7 @@ const buildDropdown = (friendsArray) => {
   </button>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">`;
   friendsArray.forEach((friend) => {
-    dropdown += `<div class="dropdown-item">${friend.name}</div>`;
+    dropdown += `<div class="dropdown-item" data-dropdown-id=${friend.id}>${friend.name}</div>`;
   });
   dropdown += '</div></div>';
   $('#dropdown-container').html(dropdown);
@@ -38,4 +68,14 @@ const friendsPage = () => {
       console.error('error in getting friends', error);
     });
 };
-export default friendsPage;
+
+const bindEvents = () => {
+  $('body').on('click', '.dropdown-item', getSingleFriend);
+};
+
+const initializeFriendsPage = () => {
+  friendsPage();
+  bindEvents();
+};
+
+export default initializeFriendsPage;
