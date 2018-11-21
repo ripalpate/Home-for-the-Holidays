496c9eeeb1b4177f8a6619533f415ffa5c5218ed
Ripal Patel <ripal212007@gmail.com>
2018-11-20 19:58:48 -0600

    successfully adding friend

--- src/components/addEditFriends/addEditFriends.js
+++ src/components/addEditFriends/addEditFriends.js
@@ -1,32 +1,29 @@
 import $ from 'jquery';
 import authHelpers from '../../helpers/authHelpers';
+import friendsData from '../../helpers/data/friendsData';
+import initializeFriendsPage from '../friendsPage/friendsPage';
 
 const formBuilder = () => {
   const form = `
   <div class="form-group">
     <label for="form-friend-name">Name:</label>
     <input type="text" class="form-control" id="form-friend-name" placeholder="John Smith">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
     <label for="form-friend-address">Address:</label>
     <input type="text" class="form-control" id="form-friend-address" placeholder="500 Interstate Blvd">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
     <label for="form-friend-email">Email:</label>
     <input type="email" class="form-control" id="form-friend-email" placeholder="fake@person.com">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
     <label for="form-friend-phone">Phone Number:</label>
     <input type="text" class="form-control" id="form-friend-phone" placeholder="111-111-2222">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
     <label for="form-friend-relationship">Relationship:</label>
     <input type="text" class="form-control" id="form-friend-relationship" placeholder="student">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   `;
   return form;
@@ -42,7 +39,31 @@ const gettingFriendFromForm = () => {
     isAvoiding: false,
     uid: authHelpers.getCurrentUid(),
   };
-  console.log(friend);
+  return friend;
 };
 
-export default { formBuilder, gettingFriendFromForm };
+const buildAddForm = () => {
+  let domString = '<h2> Add New Friend</h2>';
+  domString += formBuilder();
+  domString += '<button id="add-friend">Save New Friend</button>';
+  $('#add-edit-friend').html(domString).show();
+  $('#friends').hide();
+};
+
+const addNewFriend = () => {
+  const newFriend = gettingFriendFromForm();
+  friendsData.addNewFriend(newFriend)
+    .then(() => {
+      // when you add object firebase returns firebase unique id.
+      // tryadding result in .then () and see console.log(result);
+      $('#add-edit-friend').html('').hide();
+      $('#friends').show();
+      initializeFriendsPage();
+    }).catch((error) => {
+      console.error(error);
+    });
+};
+
+$('body').on('click', '#add-friend', addNewFriend);
+
+export default buildAddForm;
